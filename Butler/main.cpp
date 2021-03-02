9b26a722d8d252760d17a275a9d01287ec758362
Alexey Antropov <alexey.antropov@artplant.no>
2021-03-02 16:42:37 +0300

    initial setup

---
+++ Butler/main.cpp
@@ -0,0 +1,8 @@
+#include <wtypes.h>
+#include "ExportDef.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    
+    return 0;
+}
---
+++ Runtime/ExportDef.h
@@ -0,0 +1,5 @@
+#ifndef _SAILOR_IMPORT_
+# define SAILOR_DECLSPEC __declspec(dllexport)
+#else
+# define SAILOR_DECLSPEC __declspec(dllimport)
+#endif
