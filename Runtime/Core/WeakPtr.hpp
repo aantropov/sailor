ebf172b4b7f190428cf50fb1abe417583f70ee02
alexey.antropov <alexey.antropov1@gmail.com>
2021-10-02 01:37:06 +0300

    move semantics for weak ptr

--- Runtime/Core/WeakPtr.hpp
+++ Runtime/Core/WeakPtr.hpp
@@ -10,43 +10,66 @@ namespace Sailor
 	{
 	public:
 
-		TWeakPtr() noexcept { }
+		TWeakPtr() noexcept = default;
 
-		template<typename R,
-			typename = std::enable_if_t<
-			std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
-			TWeakPtr(const TWeakPtr<R>& pDerivedPtr) noexcept
-		{
-			AssignRawPtr(static_cast<T*>(pDerivedPtr.m_pRawPtr), pDerivedPtr.pControlBlock);
-		}
-
-		template<typename R,
-			typename = std::enable_if_t<
-			std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
-			TWeakPtr(const TSharedPtr<R>& pDerivedPtr) noexcept
-		{
-			AssignRawPtr(static_cast<T*>(pDerivedPtr.m_pRawPtr), pDerivedPtr.m_pControlBlock);
-		}
-
-		TWeakPtr(const TWeakPtr<T>& pWeakPtr) noexcept
+		// Basic copy/assignment
+		TWeakPtr(const TWeakPtr& pWeakPtr) noexcept
 		{
 			AssignRawPtr(pWeakPtr.m_pRawPtr, pWeakPtr.m_pControlBlock);
 		}
 
+		TWeakPtr(TWeakPtr&& pPtr) noexcept
+		{
+			Swap(std::move(pPtr));
+		}
+
+		TWeakPtr& operator=(TWeakPtr&& pPtr) noexcept
+		{
+			Swap(std::move(pPtr));
+			return *this;
+		}
+
+		template<typename R, typename = std::enable_if_t<std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
+		TWeakPtr(const TWeakPtr<R>& pDerivedPtr) noexcept
+		{
+			AssignRawPtr(static_cast<T*>(pDerivedPtr.m_pRawPtr), pDerivedPtr.pControlBlock);
+		}
+
+		template<typename R, typename = std::enable_if_t<std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
+		TWeakPtr(TWeakPtr<R>&& pDerivedPtr) noexcept
+		{
+			Swap(std::move(pDerivedPtr));
+		}
+
+		template<typename R, typename = std::enable_if_t<std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
+		TWeakPtr& operator=(TWeakPtr<R> pDerivedPtr) noexcept
+		{
+			Swap(std::move(pDerivedPtr));
+			return *this;
+		}
+
+		// Shared pointer
 		TWeakPtr(const TSharedPtr<T>& pSharedPtr) noexcept
 		{
 			AssignRawPtr(pSharedPtr.m_pRawPtr, pSharedPtr.m_pControlBlock);
 		}
 
-		TWeakPtr& operator=(const TWeakPtr<T>& pDerivedPtr)
+		TWeakPtr& operator=(const TSharedPtr<T>& pSharedPtr) noexcept
 		{
-			AssignRawPtr(pDerivedPtr.m_pRawPtr, pDerivedPtr.m_pControlBlock);
+			AssignRawPtr(pSharedPtr.m_pRawPtr, pSharedPtr.m_pControlBlock);
 			return *this;
 		}
 
-		TWeakPtr& operator=(const TSharedPtr<T>& pSharedPtr)
+		template<typename R, typename = std::enable_if_t<std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
+		TWeakPtr(const TSharedPtr<R>& pDerivedPtr) noexcept
 		{
-			AssignRawPtr(pSharedPtr.m_pRawPtr, pSharedPtr.m_pControlBlock);
+			AssignRawPtr(static_cast<T*>(pDerivedPtr.m_pRawPtr), pDerivedPtr.m_pControlBlock);
+		}
+
+		template<typename R, typename = std::enable_if_t<std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
+		TWeakPtr& operator=(const TSharedPtr<R>& pDerivedPtr) noexcept
+		{
+			AssignRawPtr(static_cast<T*>(pDerivedPtr.m_pRawPtr), pDerivedPtr.m_pControlBlock);
 			return *this;
 		}
 
@@ -57,15 +80,6 @@ namespace Sailor
 			TSharedPtr<T> pRes;
 			pRes.AssignRawPtr(m_pRawPtr, m_pControlBlock);
 			return pRes;
-		}
-
-		template<typename R,
-			typename = std::enable_if_t<
-			std::is_base_of_v<T, R> && !std::is_same_v<T, R>>>
-			TWeakPtr& operator=(const TWeakPtr<R>& pDerivedPtr)
-		{
-			AssignRawPtr(static_cast<T*>(pDerivedPtr.m_pRawPtr), pDerivedPtr.m_pControlBlock);
-			return *this;
 		}
 
 		operator bool() const noexcept { return m_pRawPtr != nullptr && m_pControlBlock->m_sharedPtrCounter > 0; }
@@ -132,6 +146,28 @@ namespace Sailor
 				delete m_pControlBlock;
 				m_pControlBlock = nullptr;
 			}
+		}
+
+		template<typename R, typename = std::enable_if_t<std::is_base_of_v<T, R> || std::is_same_v<T, R>>>
+		void Swap(TWeakPtr<R>&& pPtr)
+		{
+			if (m_pRawPtr == static_cast<T*>(pPtr.m_pRawPtr))
+			{
+				pPtr.m_pRawPtr = nullptr;
+				pPtr.m_pControlBlock = nullptr;
+				return;
+			}
+
+			if (m_pRawPtr)
+			{
+				DecrementRefCounter();
+			}
+
+			m_pRawPtr = pPtr.m_pRawPtr;
+			m_pControlBlock = pPtr.m_pControlBlock;
+
+			pPtr.m_pRawPtr = nullptr;
+			pPtr.m_pControlBlock = nullptr;
 		}
 
 		friend class TWeakPtr;
