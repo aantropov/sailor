d4d107fa4414fd354f19b2dea608b28149586a8c
alexey.antropov <alexey.antropov1@gmail.com>
2021-10-13 10:19:09 +0300

    Rearrange testing code

--- Runtime/Memory/Memory.cpp
+++ Runtime/Memory/Memory.cpp
@@ -20,143 +20,150 @@ void GlobalHeapAllocator::Free(void* pData, size_t size)
 	free(pData);
 }
 
+class TestMallocAllocator
+{
+public:
+	inline void* Allocate(size_t size, uint32_t alignment)
+	{
+		return malloc(size);
+	}
+
+	inline void Free(void* ptr)
+	{
+		free(ptr);
+	}
+};
+
+template<typename TAllocator>
+void TestPerformanceRandom(TAllocator& allocator, const std::vector<size_t>& sizesToAllocate, Utils::AccurateTimer& timer)
+{
+	auto firstTestElement = allocator.Allocate(1, 1);
+
+	std::vector<decltype(firstTestElement)> ptrs;
+	ptrs.resize(sizesToAllocate.size());
+
+	timer.Start();
+	uint32_t border = (uint32_t)sizesToAllocate.size() / 4;
+	for (uint32 i = 0; i < border * 2; ++i)
+	{
+		ptrs[i] = allocator.Allocate(sizesToAllocate[i], 1);
+	}
+	timer.Stop();
+
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::shuffle(ptrs.begin(), ptrs.end() - (size_t)border * 3, g);
+
+	timer.Start();
+
+	for (uint32 i = 0; i < border; ++i)
+	{
+		allocator.Free(ptrs[i]);
+	}
+
+	for (uint32 i = border * 2; i < sizesToAllocate.size(); ++i)
+	{
+		ptrs[i] = allocator.Allocate(sizesToAllocate[i], 1);
+	}
+
+	for (uint32 i = border; i < sizesToAllocate.size(); ++i)
+	{
+		allocator.Free(ptrs[i]);
+	}
+	timer.Stop();
+}
+
+template<typename TAllocator>
+void TestPerformanceShuffled(TAllocator& allocator, const std::vector<size_t>& sizesToAllocate, Utils::AccurateTimer& timer)
+{
+	auto firstTestElement = allocator.Allocate(1, 1);
+
+	std::vector<decltype(firstTestElement)> ptrs;
+	ptrs.resize(sizesToAllocate.size());
+
+	timer.Start();
+	for (uint32 i = 0; i < sizesToAllocate.size(); ++i)
+	{
+		ptrs[i] = allocator.Allocate(sizesToAllocate[i], 1);
+	}
+	timer.Stop();
+
+	std::random_device rd;
+	std::mt19937 g(rd());
+	std::shuffle(ptrs.begin(), ptrs.end(), g);
+
+	timer.Start();
+	for (uint32 i = 0; i < sizesToAllocate.size(); ++i)
+	{
+		allocator.Free(ptrs[i]);
+	}
+	timer.Stop();
+}
+
+template<typename TAllocator>
+void TestPerformanceSimple(TAllocator& allocator, const std::vector<size_t>& sizesToAllocate, Utils::AccurateTimer& timer)
+{
+	auto firstTestElement = allocator.Allocate(1, 1);
+
+	std::vector<decltype(firstTestElement)> ptrs;
+	ptrs.resize(sizesToAllocate.size());
+
+	timer.Start();
+	for (uint32 i = 0; i < sizesToAllocate.size(); ++i)
+	{
+		ptrs[i] = allocator.Allocate(sizesToAllocate[i], 1);
+	}
+	
+	for (uint32 i = 0; i < sizesToAllocate.size(); ++i)
+	{
+		allocator.Free(ptrs[i]);
+	}
+	timer.Stop();
+}
+
 void Sailor::Memory::TestPerformance()
 {
 	static const uint32 IterationsCount = 10;
 	static const uint32 AllocationsCount = 1000000;
-	static const uint32 AllocatorSize = 1024 * 1024 * 32;
 	static const uint32 MaxSize = 32;
 
-	std::vector<std::pair<void*, size_t> > objs;
-	objs.resize(AllocationsCount);
+	std::vector<size_t> sizesToAllocate;
+	sizesToAllocate.resize(AllocationsCount);
 
 	srand(0);
 
-	size_t sumSize = 0;
+	size_t totalAllocatedSize = 0;
 
-	for (uint32 i = 0; i < objs.size(); ++i)
+	for (uint32 i = 0; i < sizesToAllocate.size(); ++i)
 	{
-		objs[i].second = rand() % MaxSize + 1;
-		sumSize += objs[i].second;
+		sizesToAllocate[i] = rand() % MaxSize + 1;
+		totalAllocatedSize += sizesToAllocate[i];
 	}
 
-	printf("Total alloc memory size: %.2f Mb\n\n", (float)((double)(sumSize / 1024) / 1024.0));
+	printf("Total alloc memory size: %.2f Mb\n\n", (float)((double)(totalAllocatedSize / 1024) / 1024.0));
 
 	Utils::AccurateTimer poolAllocatorTimer;
 	Utils::AccurateTimer multiPoolAllocatorTimer;
 	Utils::AccurateTimer blockAllocatorTimer;
 	Utils::AccurateTimer mallocTimer;
 
-	mallocTimer.Start();
-	for (int n = 0; n <= IterationsCount; ++n)
+	for (uint32_t i = 0; i < IterationsCount; i++)
 	{
-		for (uint32 i = 0; i < objs.size(); ++i)
-			objs[i].first = malloc(objs[i].second);
+		TestMallocAllocator mallocAllocator;
+		TestPerformanceSimple(mallocAllocator, sizesToAllocate, mallocTimer);
 
-		mallocTimer.Stop();
+		Memory::TMultiPoolAllocator multiPoolAllocator;
+		TestPerformanceSimple(multiPoolAllocator, sizesToAllocate, multiPoolAllocatorTimer);
 
-		std::random_device rd;
-		std::mt19937 g(rd());
-		std::shuffle(objs.begin(), objs.end(), g);
+		Memory::TPoolAllocator poolAllocator(128, 32);
+		TestPerformanceSimple(poolAllocator, sizesToAllocate, poolAllocatorTimer);
 
-		mallocTimer.Start();
-		for (uint32 i = 0; i < objs.size(); ++i)
-			free(objs[i].first);
-	}
-	mallocTimer.Stop();
-
-	size_t maxSizeBlock = 0;
-	{
-		std::vector<Memory::TMemoryPtr<void*>> currentObjs;
-		currentObjs.resize(AllocationsCount);
-
-		blockAllocatorTimer.Start();
-
-		for (int n = 0; n <= IterationsCount; ++n)
-		{
-			Memory::TBlockAllocator<Memory::GlobalHeapAllocator> heapAllocator(2048, 32);
-
-			for (uint32 i = 0; i < objs.size(); ++i)
-				currentObjs[i] = heapAllocator.Allocate(objs[i].second, 1);
-
-			blockAllocatorTimer.Stop();
-
-			maxSizeBlock = heapAllocator.GetOccupiedSpace();
-
-			std::random_device rd;
-			std::mt19937 g(rd());
-			std::shuffle(currentObjs.begin(), currentObjs.end(), g);
-
-			blockAllocatorTimer.Start();
-
-			for (uint32 i = 0; i < objs.size(); ++i)
-				heapAllocator.Free(currentObjs[i]);
-		}
-
-		blockAllocatorTimer.Stop();
-	}
-
-	{
-		std::vector<Memory::TMemoryPtr<void*>> currentObjs;
-		currentObjs.resize(AllocationsCount);
-
-		multiPoolAllocatorTimer.Start();
-
-		for (int n = 0; n <= IterationsCount; ++n)
-		{
-			Memory::TMultiPoolAllocator heapAllocator;
-
-			for (uint32 i = 0; i < objs.size(); ++i)
-				currentObjs[i] = heapAllocator.Allocate(objs[i].second, 1);
-
-			multiPoolAllocatorTimer.Stop();
-
-			std::random_device rd;
-			std::mt19937 g(rd());
-			std::shuffle(currentObjs.begin(), currentObjs.end(), g);
-
-			multiPoolAllocatorTimer.Start();
-
-			for (uint32 i = 0; i < objs.size(); ++i)
-				heapAllocator.Free(currentObjs[i]);
-		}
-
-		multiPoolAllocatorTimer.Stop();
-	}
-
-	size_t maxSizePool = 0;
-	{
-		std::vector<Memory::TMemoryPtr<void*>> currentObjs;
-		currentObjs.resize(AllocationsCount);
-
-		for (int n = 0; n <= IterationsCount; ++n)
-		{
-			poolAllocatorTimer.Start();
-
-			Memory::TPoolAllocator heapAllocator(128, 32);
-
-			for (uint32 i = 0; i < objs.size(); ++i)
-				currentObjs[i] = heapAllocator.Allocate(objs[i].second, 1);
-
-			maxSizePool = std::max(maxSizePool, heapAllocator.GetOccupiedSpace());
-
-			poolAllocatorTimer.Stop();
-
-			std::random_device rd;
-			std::mt19937 g(rd());
-			std::shuffle(currentObjs.begin(), currentObjs.end(), g);
-
-			poolAllocatorTimer.Start();
-
-			for (uint32 i = 0; i < objs.size(); ++i)
-				heapAllocator.Free(currentObjs[i]);
-
-			poolAllocatorTimer.Stop();
-		}
+		Memory::TBlockAllocator blockAllocator(2048, 32);
+		TestPerformanceSimple(blockAllocator, sizesToAllocate, blockAllocatorTimer);
 	}
 
 	printf("mallocAllocator %lld ms\n", mallocTimer.ResultAccumulatedMs());
-	printf("blockAllocator %lld ms, total space %lld kb \n", blockAllocatorTimer.ResultAccumulatedMs(), maxSizeBlock / 1024);
-	printf("poolAllocator %lld ms, total space %lld kb \n", poolAllocatorTimer.ResultAccumulatedMs(), maxSizePool / 1024);
+	printf("blockAllocator %lld ms\n", blockAllocatorTimer.ResultAccumulatedMs());
+	printf("poolAllocator %lld ms\n", poolAllocatorTimer.ResultAccumulatedMs());
 	printf("multiPoolAllocator %lld ms\n", multiPoolAllocatorTimer.ResultAccumulatedMs());
 }
