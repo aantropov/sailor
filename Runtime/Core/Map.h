82c56cee783ab3695348f5a71bc4e266e6469511
alexey.antropov <alexey.antropov@thetoadman.com>
2021-12-15 19:52:15 +0300

    Start map

--- Runtime/Core/Map.h
+++ Runtime/Core/Map.h
@@ -10,5 +10,31 @@
 
 namespace Sailor
 {
+	template<typename TKeyType, typename TValueType, typename TAllocator>
+	class TMap
+	{
+	public:
+
+		using TKeyConstPointerType = TKey const*;
+		using TValueConstPointerType = TKey const*;
+
+		TMap() = default;
+		TMap(TMap&&) = default;
+		TMap(const TMap&) = default;
+		TMap& operator=(TMap&&) = default;
+		TMap& operator=(const TMap&) = default;
+
+		TValueType& operator[](TKeyConstPointerType key) { return FindChecked(key); }
+		const TValueType& operator[](TKeyConstPointerType key) const { return FindChecked(key); }
+
+	protected:
+
+		TValueType& FindChecked(TKeyConstPointerType key)
+		{
+			return nullptr;
+		}
+
+		TAllocator m_allocator{};
+	};
 
 }
