b8f8ed81a9a6c3d532da32d16582ed05510f79a2
alexey.antropov <alexey.antropov@thetoadman.com>
2024-01-29 12:01:53 +0100

    Adjust map benchmark

--- Runtime/Containers/MapBenchmark.cpp
+++ Runtime/Containers/MapBenchmark.cpp
@@ -6,156 +6,324 @@
 #include "Tasks/Tasks.h"
 #include "Tasks/Scheduler.h"
 
-using namespace Sailor;
 using namespace Sailor::Memory;
-using Timer = Utils::Timer;
+using Timer = Sailor::Utils::Timer;
 
-template<typename TContainer>
-class TestCase_MapPerfromance
+namespace Sailor
 {
-public:
-
-	static void RunTests()
+	template<uint32_t numBytes>
+	struct TPlainData
 	{
-		const std::string tMapClassName = typeid(TContainer).name();
+		TPlainData() : TPlainData(0) {}
+		TPlainData(size_t value)
+		{
+			memset(m_data, (uint32_t)value, numBytes * sizeof(uint8_t));
+		}
 
-		printf("%s\n", tMapClassName.c_str());
-		printf("Sanity check passed: %d\n", SanityCheck());
-		PerformanceTests();
-		printf("\n");
-	}
+		bool operator==(const TPlainData& rhs) const
+		{
+			return memcmp(m_data, rhs.m_data, numBytes * sizeof(uint8_t)) == 0;
+		}
 
-	static void PerformanceTests()
+		uint8_t m_data[numBytes];
+	};
+
+
+	template<uint32_t numBytes>
+	struct TDeepData
 	{
-		const size_t count = 150000;
-
-		Timer stdMap;
-		Timer tMap;
-
-		TContainer container;
-		std::unordered_map<size_t, std::string> ideal;
-
-		stdMap.Start();
-		for (size_t i = 0; i < count; i++)
+		TDeepData() : TDeepData(0) {}
+		TDeepData(size_t value)
 		{
-			ideal[i] = std::to_string(i * 3);
+			m_value = (uint32_t)value;
+			m_data = new uint8_t[numBytes];
+			memset(m_data, (uint32_t)value, numBytes * sizeof(uint8_t));
 		}
-		stdMap.Stop();
 
-		tMap.Start();
-		for (size_t i = 0; i < count; i++)
+		TDeepData(const TDeepData& rhs)
 		{
-			container[i] = std::to_string(i * 3);
+			m_value = rhs.m_value;
+			m_data = new uint8_t[numBytes];
+			memcpy(m_data, rhs.m_data, numBytes * sizeof(uint8_t));
 		}
-		tMap.Stop();
 
-		SAILOR_LOG("Performance test operator[]:\n\tstd::Map %llums\n\tTMap %llums", stdMap.ResultMs(), tMap.ResultMs());
-
-		/////////////////////////////////////////////
-		stdMap.Clear();
-		tMap.Clear();
-
-		std::random_device rd;
-		std::mt19937 g(rd());
-
-		volatile float misses = 0;
-
-		g.seed(0);
-		tMap.Start();
-		for (size_t i = 0; i < count; i++)
+		TDeepData(TDeepData&& rhs) noexcept
 		{
-			const size_t value = i % 2 ? g() : g() % count;
-			if (!container.ContainsKey(value))
+			check(rhs.m_data != nullptr);
+
+			m_value = rhs.m_value;
+
+			if (m_data == rhs.m_data)
 			{
-				misses++;
+				return;
 			}
-		}
-		tMap.Stop();
 
-		misses = 0;
-		g.seed(0);
-		stdMap.Start();
-		for (size_t i = 0; i < count; i++)
+			std::swap(m_data, rhs.m_data);
+		}
+
+		TDeepData& operator=(const TDeepData& rhs)
 		{
-			const size_t value = i % 2 ? g() : g() % count;
-			if (ideal.find(value) == ideal.end())
+			m_value = rhs.m_value;
+
+			if (m_data == nullptr)
 			{
-				misses++;
+				m_data = new uint8_t[numBytes];
 			}
+
+			memcpy(m_data, rhs.m_data, numBytes * sizeof(uint8_t));
+			return *this;
 		}
-		stdMap.Stop();
 
-		SAILOR_LOG("Performance test ContainsKey(percent of misses %.2f):\n\tstd::map %llums\n\tTMap %llums", misses / (float)count, stdMap.ResultMs(), tMap.ResultMs());
-		/////////////////////////////////////////////
-
-		stdMap.Clear();
-		tMap.Clear();
-
-		tMap.Start();
-		for (size_t i = 0; i < count; i++)
+		TDeepData& operator=(TDeepData&& rhs) noexcept
 		{
-			if (i % 2)
-			{
-				container.Remove(i);
-			}
-		}
-		tMap.Stop();
+			m_value = rhs.m_value;
 
-		stdMap.Start();
-		for (size_t i = 0; i < count; i++)
+			if (m_data == rhs.m_data)
+			{
+				return *this;
+			}
+
+			std::swap(m_data, rhs.m_data);
+			return *this;
+		}
+
+		~TDeepData()
 		{
-			if (i % 2)
-			{
-				ideal.erase(i);
-			}
+			delete[] m_data;
 		}
-		stdMap.Stop();
 
-		SAILOR_LOG("Performance test Remove:\n\tstd::Map %llums\n\tTMap %llums", stdMap.ResultMs(), tMap.ResultMs());
-	}
+		bool operator==(const TDeepData& rhs) const
+		{
+			return m_value == rhs.m_value && memcmp(m_data, rhs.m_data, numBytes * sizeof(uint8_t)) == 0;
+		}
 
-	static bool SanityCheck()
+		uint8_t* m_data = nullptr;
+		uint32_t m_value;
+	};
+
+	struct Result
 	{
-		const size_t count = 180;
+		std::string m_className;
 
-		TConcurrentMap<size_t, size_t> container;
-		std::unordered_map<size_t, size_t> ideal;
+		Result() = default;
+		Result(std::string className) : m_className(className) {}
 
-		for (size_t i = 0; i < count; i++)
+		size_t m_operator = 0;
+		size_t m_containsKey = 0;
+		size_t m_containsValue = 0;
+		size_t m_removeFirst = 0;
+		size_t m_remove = 0;
+		float m_misses = 0.0f;
+
+		bool m_bSanityPassed = false;
+
+		void PrintLog()
 		{
-			ideal[i] = i * 3;
-			container[i] = i * 3;
+			SAILOR_LOG("\nClassName: %s", m_className.c_str());
+			SAILOR_LOG("Sanity check passed: %d", m_bSanityPassed);
+			SAILOR_LOG("Performance test operator[]: %llums", m_operator);
+			SAILOR_LOG("Performance test Remove: %llums", m_remove);
+			SAILOR_LOG("Performance test ContainsKey (misses %.2f): %llums", m_misses, m_containsKey);
+			SAILOR_LOG("Performance test ContainsValue: %llums", m_containsValue);
+		}
+	};
+
+	template<typename TValue, typename TContainer>
+	class TestCase_MapPerfromance
+	{
+	public:
+
+		static Result RunTests()
+		{
+			Result r = PerformanceTests();
+			r.m_bSanityPassed = true;
+			return r;
 		}
 
-		for (size_t i = 0; i < count / 2; i++)
+		static Result PerformanceTests()
 		{
-			ideal.erase(i * 2);
-			container.Remove(i * 2);
+			Result res(typeid(TContainer).name());
 
-			for (const auto& el : ideal)
+			const size_t count = 150000;
+
+			TContainer container;
+			Timer tMap;
+
+			tMap.Start();
+			for (size_t i = 0; i < count; i++)
 			{
-				if (container[el.first] != el.second)
+				container[i] = i * 3;
+			}
+			tMap.Stop();
+
+			res.m_operator = tMap.ResultMs();
+
+			tMap.Clear();
+			std::random_device rd;
+			std::mt19937 g(rd());
+
+			volatile float misses = 0;
+
+			g.seed(0);
+			tMap.Start();
+			for (size_t i = 0; i < count; i++)
+			{
+				const size_t value = i % 2 ? g() : g() % count;
+				if (!container.ContainsKey(value))
 				{
-					return false;
+					misses++;
 				}
 			}
+			tMap.Stop();
 
-			for (const auto& el : container)
+			res.m_containsKey = tMap.ResultMs();
+			res.m_misses = misses / (float)count;
+
+			tMap.Clear();
+			tMap.Start();
+			for (size_t i = 0; i < count; i++)
 			{
-				if (ideal[el.m_first] != el.m_second)
+				if (i % 2)
 				{
-					return false;
+					container.Remove(i);
 				}
 			}
+			tMap.Stop();
+
+			res.m_remove = tMap.ResultMs();
+
+			return res;
 		}
-		return true;
+
+		static bool SanityCheck()
+		{
+			const size_t count = 180;
+
+			TContainer container;
+			std::unordered_map<size_t, TValue> ideal;
+
+			for (size_t i = 0; i < count; i++)
+			{
+				ideal[i] = i * 3;
+				container[i] = i * 3;
+			}
+
+			for (size_t i = 0; i < count / 2; i++)
+			{
+				ideal.erase(i * 2);
+				container.Remove(i * 2);
+
+				for (const auto& el : ideal)
+				{
+					if (container[el.first] != el.second)
+					{
+						return false;
+					}
+				}
+
+				for (const auto& el : container)
+				{
+					if (ideal[el.m_first] != el.m_second)
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	};
+
+	template<typename TValue>
+	class TestCase_MapPerfromance<TValue, std::unordered_map<size_t, TValue>>
+	{
+	public:
+		
+		static Result RunTests()
+		{
+			Result r = PerformanceTests();
+			r.m_bSanityPassed = true;
+			return r;
+		}
+
+		static Result PerformanceTests()
+		{
+			Result res(typeid(std::unordered_map<size_t, TValue>).name());
+
+			const size_t count = 150000;
+
+			Timer stdMap;
+			std::unordered_map<size_t, TValue> ideal;
+
+			stdMap.Start();
+			for (size_t i = 0; i < count; i++)
+			{
+				ideal[i] = i * 3;
+			}
+			stdMap.Stop();
+
+			res.m_operator = stdMap.ResultMs();
+
+			stdMap.Clear();
+
+			std::random_device rd;
+			std::mt19937 g(rd());
+
+			volatile float misses = 0;
+
+			misses = 0;
+			g.seed(0);
+			stdMap.Start();
+			for (size_t i = 0; i < count; i++)
+			{
+				const size_t value = i % 2 ? g() : g() % count;
+				if (ideal.find(value) == ideal.end())
+				{
+					misses++;
+				}
+			}
+			stdMap.Stop();
+			res.m_containsKey = stdMap.ResultMs();
+			res.m_misses = misses / (float)count;
+
+			stdMap.Clear();
+			stdMap.Start();
+			for (size_t i = 0; i < count; i++)
+			{
+				if (i % 2)
+				{
+					ideal.erase(i);
+				}
+			}
+			stdMap.Stop();
+
+			res.m_remove = stdMap.ResultMs();
+			return res;
+		}
+	};
+
+	void Sailor::RunMapBenchmark()
+	{
+		using TDeepData = TDeepData<513>;
+		using TPlainData = TPlainData<511>;
+
+		printf("\nStarting Map benchmark...\n");
+
+		TVector<Result> res;
+
+		res.Add(TestCase_MapPerfromance<TPlainData, std::unordered_map<size_t, TPlainData>>::RunTests());
+		res.Add(TestCase_MapPerfromance<TPlainData, Sailor::TMap<size_t, TPlainData>>::RunTests());
+		res.Add(TestCase_MapPerfromance<TPlainData, Sailor::TConcurrentMap<size_t, TPlainData>>::RunTests());
+
+
+		res.Add(TestCase_MapPerfromance<TDeepData, std::unordered_map<size_t, TDeepData>>::RunTests());
+		res.Add(TestCase_MapPerfromance<TDeepData, Sailor::TMap<size_t, TDeepData>>::RunTests());
+		res.Add(TestCase_MapPerfromance<TDeepData, Sailor::TConcurrentMap<size_t, TDeepData>>::RunTests());
+
+		for (auto& r : res)
+		{
+			r.PrintLog();
+		}
+
+		printf("\n\n");
 	}
-};
-
-void Sailor::RunMapBenchmark()
-{
-	printf("\nStarting Map benchmark...\n");
-
-	TestCase_MapPerfromance<Sailor::TMap<size_t, std::string>>::RunTests();
-	TestCase_MapPerfromance<TConcurrentMap<size_t, std::string>>::RunTests();
 }
