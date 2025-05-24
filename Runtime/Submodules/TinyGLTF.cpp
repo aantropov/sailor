d79f6aedf1dcc4bce95ffb7d73159eaac2909956
aantropov <alexey.antropov1@gmail.com>
2025-05-24 22:05:15 +0200

    Fix tinygltf implementation

---
+++ Runtime/Submodules/TinyGLTF.cpp
@@ -0,0 +1,10 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBI_MSC_SECURE_CRT
+#define __STDC_LIB_EXT1__
+
+#include <stb_image.h>
+#include <stb_image_write.h>
+
+#define TINYGLTF_IMPLEMENTATION
+#include <tiny_gltf.h>
