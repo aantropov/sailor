a1d637c7c1e84823f6f99f4727863cb9d0955315
aantropov <alexey.antropov1@gmail.com>
2026-04-20 11:58:29 +0200

    Improve content panel current-folder browsing

--- Editor/Views/ContentFolderView.xaml.cs
+++ Editor/Views/ContentFolderView.xaml.cs
@@ -27,12 +27,20 @@ namespace SailorEditor.Views
             dispatcher = MauiProgram.GetService<ICommandDispatcher>();
             contextProvider = MauiProgram.GetService<IActionContextProvider>();
 
+            SortPicker.ItemsSource = Enum.GetValues<ProjectContentSortMode>().Cast<object>().ToList();
+            SortPicker.SelectedItem = contentStore.State.SortMode;
+            FilterSearchBar.Text = contentStore.State.Filter;
+
             PopulateTreeView(contentStore.Projection);
 
             contentStore.ProjectionChanged += PopulateTreeView;
             FolderTree.SelectedItemChanged += OnSelectTreeViewNode;
             FolderTree.DropRequested += OnContentDropRequested;
             FolderTree.ContextRequested += OnContentContextRequested;
+            CurrentFoldersView.SelectionChanged += OnCurrentFolderSelectionChanged;
+            CurrentAssetsView.SelectionChanged += OnCurrentAssetSelectionChanged;
+            FilterSearchBar.TextChanged += OnFilterTextChanged;
+            SortPicker.SelectedIndexChanged += OnSortChanged;
 
             var selectionViewModel = MauiProgram.GetService<SelectionService>();
             selectionViewModel.OnSelectAssetAction += SelectAssetFile;
@@ -46,6 +54,7 @@ namespace SailorEditor.Views
                 try
                 {
                     FolderTree.SelectedItem = null;
+                    CurrentAssetsView.SelectedItem = null;
                 }
                 finally
                 {
@@ -63,6 +72,72 @@ namespace SailorEditor.Views
             contentStore.SelectAsset(file.FileId.Value);
         }
 
+        void OnFilterTextChanged(object? sender, TextChangedEventArgs e)
+        {
+            if (string.Equals(e.NewTextValue, contentStore.State.Filter, StringComparison.Ordinal))
+            {
+                return;
+            }
+
+            contentStore.SetFilter(e.NewTextValue);
+        }
+
+        void OnSortChanged(object? sender, EventArgs e)
+        {
+            if (SortPicker.SelectedItem is not ProjectContentSortMode sortMode || sortMode == contentStore.State.SortMode)
+            {
+                return;
+            }
+
+            contentStore.SetSort(sortMode);
+        }
+
+        void OnCurrentFolderSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (suppressSelectionChanged)
+            {
+                return;
+            }
+
+            if (e.CurrentSelection.FirstOrDefault() is ProjectContentFolderItem folder)
+            {
+                contentStore.SelectFolder(folder.FolderId);
+            }
+
+            CurrentFoldersView.SelectedItem = null;
+        }
+
+        void OnCurrentAssetSelectionChanged(object? sender, SelectionChangedEventArgs e)
+        {
+            if (suppressSelectionChanged)
+            {
+                return;
+            }
+
+            if (e.CurrentSelection.FirstOrDefault() is ProjectContentAssetItem asset)
+            {
+                OpenAssetById(asset.FileId);
+            }
+
+            CurrentAssetsView.SelectedItem = null;
+        }
+
+        void OpenAssetById(string fileId)
+        {
+            if (!service.Files.Any(x => x.FileId?.Value == fileId))
+            {
+                return;
+            }
+
+            var assetFile = service.Files.First(x => x.FileId?.Value == fileId);
+            contentStore.SelectAsset(fileId);
+            dispatcher.DispatchAsync(
+                    new OpenAssetCommand(assetFile),
+                    contextProvider.GetCurrentContext(new CommandOrigin(CommandOriginKind.Panel, nameof(ContentFolderView))))
+                .GetAwaiter()
+                .GetResult();
+        }
+
         void OnSelectTreeViewNode(object sender, EventArgs args)
         {
             if (suppressSelectionChanged)
@@ -74,16 +149,15 @@ namespace SailorEditor.Views
             switch (selectionChanged?.Model)
             {
                 case TreeViewItem<AssetFile> assetFile:
-                    MauiProgram.GetService<ProjectContentStore>().SelectAsset(assetFile.Model.FileId?.Value);
-                    MauiProgram.GetService<ICommandDispatcher>()
-                        .DispatchAsync(
+                    contentStore.SelectAsset(assetFile.Model.FileId?.Value);
+                    dispatcher.DispatchAsync(
                             new OpenAssetCommand(assetFile.Model),
-                            MauiProgram.GetService<IActionContextProvider>().GetCurrentContext(new CommandOrigin(CommandOriginKind.Panel, nameof(ContentFolderView))))
+                            contextProvider.GetCurrentContext(new CommandOrigin(CommandOriginKind.Panel, nameof(ContentFolderView))))
                         .GetAwaiter()
                         .GetResult();
                     break;
                 case TreeViewItemGroup<AssetFolder, AssetFile> folder:
-                    MauiProgram.GetService<ProjectContentStore>().SelectFolder(folder.Model?.Id);
+                    contentStore.SelectFolder(folder.Model?.Id);
                     break;
             }
         }
@@ -181,6 +255,7 @@ namespace SailorEditor.Views
 
         void PopulateTreeView(ProjectContentProjection projection)
         {
+            var expandedFolderIds = CaptureExpandedFolderIds(FolderTree.RootNodes);
             var foldersById = service.Folders.ToDictionary(x => x.Id);
             var assetsByFileId = service.Files
                 .Where(x => x.FileId is not null && !x.FileId.IsEmpty())
@@ -189,7 +264,40 @@ namespace SailorEditor.Views
             var foldersModel = FolderTreeViewBuilder.PopulateDirectory(projection, foldersById, assetsByFileId);
             var rootNodes = Controls.TreeView.PopulateGroup(foldersModel, new TreeViewPopulateArgs());
             FolderTree.RootNodes = rootNodes;
+            RestoreExpandedFolders(expandedFolderIds);
             RestoreSelection(projection);
+            UpdateBrowserSurface(projection);
+        }
+
+        void UpdateBrowserSurface(ProjectContentProjection projection)
+        {
+            var currentFolder = GetCurrentFolder(projection);
+            CurrentFolderTitle.Text = currentFolder.Name;
+            CurrentFolderPath.Text = currentFolder.FullPath;
+            CurrentFolderSummary.Text = $"{projection.CurrentFolderFolders.Count} folders • {projection.CurrentFolderAssets.Count} assets";
+
+            if (!string.Equals(FilterSearchBar.Text, projection.State.Filter, StringComparison.Ordinal))
+            {
+                FilterSearchBar.Text = projection.State.Filter;
+            }
+
+            if (SortPicker.SelectedItem is not ProjectContentSortMode selectedSort || selectedSort != projection.State.SortMode)
+            {
+                SortPicker.SelectedItem = projection.State.SortMode;
+            }
+
+            CurrentFoldersView.ItemsSource = projection.CurrentFolderFolders;
+            CurrentAssetsView.ItemsSource = projection.CurrentFolderAssets;
+        }
+
+        ProjectContentFolderItem GetCurrentFolder(ProjectContentProjection projection)
+        {
+            if (projection.State.CurrentFolderId is not { } folderId)
+            {
+                return projection.Root;
+            }
+
+            return projection.Folders.FirstOrDefault(x => x.FolderId == folderId) ?? projection.Root;
         }
 
         void RestoreSelection(ProjectContentProjection projection)
@@ -213,6 +321,7 @@ namespace SailorEditor.Views
                     return;
                 }
 
+                ExpandAncestors(selectedNode);
                 selectedNode.Select();
                 FolderTree.SelectedItem = selectedNode;
             }
@@ -248,6 +357,45 @@ namespace SailorEditor.Views
             }
 
             return null;
+        }
+
+        static HashSet<int> CaptureExpandedFolderIds(IEnumerable<TreeViewNode> nodes)
+        {
+            var expandedFolderIds = new HashSet<int>();
+            foreach (var node in nodes)
+            {
+                if (node.IsExpanded &&
+                    node.BindingContext is TreeViewItemGroup<AssetFolder, AssetFile> folder &&
+                    folder.Model is not null)
+                {
+                    expandedFolderIds.Add(folder.Model.Id);
+                }
+
+                foreach (var childFolderId in CaptureExpandedFolderIds(node.ChildrenList))
+                {
+                    expandedFolderIds.Add(childFolderId);
+                }
+            }
+
+            return expandedFolderIds;
+        }
+
+        void RestoreExpandedFolders(HashSet<int> expandedFolderIds)
+        {
+            foreach (var folderId in expandedFolderIds)
+            {
+                FindFolderNode(folderId)?.SetExpandedSilently(true);
+            }
+        }
+
+        static void ExpandAncestors(TreeViewNode node)
+        {
+            var parent = node.ParentTreeViewItem;
+            while (parent is not null)
+            {
+                parent.SetExpandedSilently(true);
+                parent = parent.ParentTreeViewItem;
+            }
         }
     }
 }
